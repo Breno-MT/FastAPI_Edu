3aba511171e8394b8880392414e2a685722d753d
Breno-MT <hentgenp@gmail.com>
2024-10-23 18:35:28 -0300

    FEAT: added new test, response added into the test functions instead of being global

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,8 +4,21 @@ from fast_zero.app import app
 
 client = TestClient(app)
 
-response = client.get('/')
 
 def test_root_must_return_ok_and_hello_world():
+    response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+def test_create_new_user_and_check_the_info():
+    response = client.post('/users/create_user', json={
+        "username": "testdasilva",
+        "email": "testdasilva@gmail.com",
+        "password": "1230isoda"
+    })
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "id": response.json().get("id") >=1,
+        "username": "testdasilva",
+        "email": "testdasilva@gmail.com"
+    }
