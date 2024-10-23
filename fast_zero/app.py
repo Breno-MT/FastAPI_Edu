1f1438f38596e2f8792ea4210759c2d8c24edf7e
Breno-MT <hentgenp@gmail.com>
2024-10-23 18:52:35 -0300

    FEAT: added create_user route, added temporary database (too trash to called it db but wtv)

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,10 +2,24 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from .schemas import Message
+from .schemas import Message, UserSchema, UserPublic, UserDB
 
 app = FastAPI()
+
+# Temporary fake database to realise some tests
+database = []
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
+
+@app.post('/users/create_user', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        id=len(database) + 1,
+        **user.model_dump()
+    )
+    
+    database.append(user_with_id)
+
+    return user_with_id
