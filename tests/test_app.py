98558014ba27f915a39a5f58b3294ea56d8d40a3
Breno-MT <hentgenp@gmail.com>
2024-10-23 18:46:14 -0300

    REFACT: removed all redundant code and start using fixtures

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,11 @@
 from http import HTTPStatus
-from fastapi.testclient import TestClient
-from fast_zero.app import app
 
-client = TestClient(app)
-
-
-def test_root_must_return_ok_and_hello_world():
+def test_root_must_return_ok_and_hello_world(client):
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
 
-def test_create_new_user_and_check_the_info():
+def test_create_new_user_and_check_the_info(client):
     response = client.post('/users/create_user', json={
         "username": "testdasilva",
         "email": "testdasilva@gmail.com",
