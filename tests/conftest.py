209cb903b075520bc7e02175b7af9ec45bd4d285
Breno-MT <hentgenp@gmail.com>
2024-10-24 19:52:22 -0300

    FEAT: added session as pytest fixture for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,20 @@
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
 from fast_zero.app import app
+from fast_zero.models import table_registry
 from fastapi.testclient import TestClient
 
 @pytest.fixture()
 def client():
     return TestClient(app)
+
+@pytest.fixture()
+def session():
+    engine = create_engine("sqlite:///:memory:")
+    table_registry.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
