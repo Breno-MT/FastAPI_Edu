2febf34a818931030d1e2010666b693567e3db5b
Breno-MT <hentgenp@gmail.com>
2024-10-28 07:36:40 -0300

    REFACT: removed fake database and implemented SQLite on endpoints, changed logics to adapt to SQLite instead of fake database array

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,70 +1,94 @@
 from http import HTTPStatus
+from sqlalchemy import select
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 
-from .schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from .database import get_session
+from .models import User
+from .schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
-# Temporary fake database to realise some tests
-database = []
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
 
 @app.post('/users/create_user', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(
-        id=len(database) + 1,
-        **user.model_dump()
+def create_user(user: UserSchema, session=Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where((User.username == user.username) | (User.email == user.email))
     )
-    
-    database.append(user_with_id)
 
-    return user_with_id
+    if db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail="Username/Email already on Database"
+        )
+
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def get_users():
-    return {"users": database}
+def get_users(limit: int = 10, offset: int = 0, session = Depends(get_session)):
+    users_db = session.scalars(
+        select(User).limit(limit).offset(offset)
+    )
+    return {"users": users_db}
 
 @app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def get_user(user_id: int):
-    user_with_id = None
-    for user in database:
-        if user.id == user_id:
-            user_with_id = user
+def get_user(user_id: int, session = Depends(get_session)):
+    user_db = session.scalar(
+        select(User).where(User.id == user_id)
+    )
     
-    if not user_with_id:
+    if not user_db:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail="User not found"
         )
     
-    return user_with_id
+    return user_db
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
+def update_user(user_id: int, user: UserSchema, session = Depends(get_session)):
 
-    if user_id < 1 or user_id > len(database):
+    user_db = session.scalar(
+        select(User).where(User.id == user_id)
+    )
+
+    if not user_db:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail="User not found"
         )
 
-    user_with_id = UserDB(
-        **user.model_dump(),
-        id=user_id
-    )
-    database[user_id - 1] = user_with_id
+    user_db.username = user.username
+    user_db.email = user.email
+    user_db.password = user.password
 
-    return user_with_id
+    session.commit()
+    session.refresh(user_db)
+
+    return user_db
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int):
-    if user_id < 1 or user_id > len(database):
+def delete_user(user_id: int, session = Depends(get_session)):
+    user_db = session.scalar(
+        select(User).where(User.id == user_id)
+    )
+
+    if not user_db:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail="User not found"
         )
-
-    del database[user_id - 1]
+    
+    session.delete(user_db)
+    session.commit()
 
     return {'message': 'User deleted successfully.'}
