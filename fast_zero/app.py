88003b7a59fc530220604af7e7b95a3873585cbd
Breno-MT <hentgenp@gmail.com>
2024-10-28 11:22:42 -0300

    REFACT: changed all endpoint to accept token JWT, changed logics from update/delete users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -6,13 +6,13 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from .database import get_session
 from .models import User
-from .security import get_password_hash, verify_password
-from .schemas import Message, UserList, UserPublic, UserSchema
+from .security import get_password_hash, verify_password, create_access_token, get_current_user
+from .schemas import Message, Token, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/token')
+@app.post('/token', response_model=Token)
 def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     session = Depends(get_session)
@@ -21,20 +21,23 @@ def login_for_access_token(
         select(User).where(User.username == form_data.username)
     )
 
-    if not user_db or verify_password(form_data.password, user_db.password):
+    if not user_db or not verify_password(form_data.password, user_db.password):
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
             detail="Incorrect username or password"
         )
     
-    return user_db
+    access_token = create_access_token(data={"sub": user_db.username})
+
+    return {"access_token": access_token, "token_type": "Bearer"}
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
 
 @app.post('/users/create_user', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session=Depends(get_session)):
+def create_user(user: UserSchema,
+                session=Depends(get_session)):
     db_user = session.scalar(
         select(User).where((User.username == user.username) | (User.email == user.email))
     )
@@ -56,14 +59,20 @@ def create_user(user: UserSchema, session=Depends(get_session)):
     return db_user
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def get_users(limit: int = 10, offset: int = 0, session = Depends(get_session)):
+def get_users(limit: int = 10,
+              offset: int = 0,
+              session = Depends(get_session),
+              current_user = Depends(get_current_user)):
+
     users_db = session.scalars(
         select(User).limit(limit).offset(offset)
     )
     return {"users": users_db}
 
-@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
-def get_user(user_id: int, session = Depends(get_session)):
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic,)
+def get_user(user_id: int,
+             session = Depends(get_session),
+             current_user = Depends(get_current_user)):
     user_db = session.scalar(
         select(User).where(User.id == user_id)
     )
@@ -76,39 +85,39 @@ def get_user(user_id: int, session = Depends(get_session)):
     return user_db
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema, session = Depends(get_session)):
+def update_user(user_id: int,
+                user: UserSchema,
+                session = Depends(get_session),
+                current_user = Depends(get_current_user)):
 
-    user_db = session.scalar(
-        select(User).where(User.id == user_id)
-    )
-
-    if not user_db:
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="Not enough permission"
         )
 
-    user_db.username = user.username
-    user_db.email = user.email
-    user_db.password = get_password_hash(user.password)
-    user_db.updated_at = func.now()
-
+    current_user.username = user.username
+    current_user.email = user.email
+    current_user.password = get_password_hash(user.password)
+    current_user.updated_at = func.now()
+    
     session.commit()
-    session.refresh(user_db)
+    session.refresh(current_user)
 
-    return user_db
+    return current_user
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int, session = Depends(get_session)):
-    user_db = session.scalar(
-        select(User).where(User.id == user_id)
-    )
-
-    if not user_db:
+def delete_user(user_id: int,
+                session = Depends(get_session),
+                current_user = Depends(get_current_user)):
+    
+    if current_user.id != user_id:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="Not enough permission"
         )
     
-    session.delete(user_db)
+    session.delete(current_user)
     session.commit()
 
     return {'message': 'User deleted successfully.'}
